0ef47ba04c8559f9ae1e1f1b54d1063e9edf7216
Jennifer Bacon <jenniferbacon01@gmail.com>
2017-10-23 17:49:43 +0100

    agile

--- test/test_bank.py
+++ test/test_bank.py
@@ -3,7 +3,7 @@ from app.bank import Bank
 
 class TestBank(unittest.TestCase):
 
-    def test_bank_add_method_returns_correct_result(self):
+    def test_bank_can_receive_a_transaction(self):
         bank = Bank()
         result = bank.add(2,2)
         self.assertEqual(4, result )
