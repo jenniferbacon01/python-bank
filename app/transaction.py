d5798f5f1980577a4dd150f37c81b2ce4cdbdbae
Jennifer Bacon <jenniferbacon01@gmail.com>
2017-10-23 13:59:38 +0100

    transaction can determine if credit or debit

--- app/transaction.py
+++ app/transaction.py
@@ -3,3 +3,9 @@ class Transaction(object):
     def __init__(self, date, amount):
         self.date = date
         self.amount = amount
+
+    def is_a_credit(self):
+        if self.amount > 0:
+            return True
+        else:
+            return False
--- test/test_transaction.py
+++ test/test_transaction.py
@@ -1,12 +1,21 @@
 import unittest
 from app.transaction import Transaction
 
-class TestBank(unittest.TestCase):
+class TestTransaction(unittest.TestCase):
 
     def test_transaction_can_be_instantiated_with_date_and_amount(self):
         transaction = Transaction("23/10/17", 100)
         self.assertEqual(transaction.date, "23/10/17" )
-        self.assertEqual(transaction.amount, 100 )
+        self.assertEqual(transaction.amount, 100)
+
+
+    def test_transaction_can_calculate_if_credit(self):
+        transaction = Transaction("23/10/17", 100)
+        self.assertTrue(transaction.is_a_credit())
+
+    def test_transaction_can_calculate_if_not_credit(self):
+        transaction = Transaction("23/10/17", -100)
+        self.assertFalse(transaction.is_a_credit())
 
 if __name__ == '__main__':
     unittest.main()
